c1d2f1724be792e68969744843cbf5b6906459e0
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 14:05:23 +1300

    Updates

---
+++ precache-manifest.284afd4d31c431d6aed725680501ec92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf13c6eaa2b757c9877ddc2d7d40079",
+    "url": "/React-App-Forms/index.html"
+  },
+  {
+    "revision": "b9d11c6e0de77fdefa83",
+    "url": "/React-App-Forms/static/css/main.3c4e2852.chunk.css"
+  },
+  {
+    "revision": "ecfcba9157697085846e",
+    "url": "/React-App-Forms/static/js/2.5044d8c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-Forms/static/js/2.5044d8c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d11c6e0de77fdefa83",
+    "url": "/React-App-Forms/static/js/main.ad77f904.chunk.js"
+  },
+  {
+    "revision": "453128587e492b535425",
+    "url": "/React-App-Forms/static/js/runtime-main.eb3b6287.js"
+  }
+]);
